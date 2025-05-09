e1465d46a9dc8f28fbecd68e2517205f8362c93b
danielo2318 <aroctaviodaniel9@gmail.com>
2025-05-08 22:19:57 -0600

    James Bond

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    Console.Write("What is your first name? ");
+    String name = Console.ReadLine();
+    Console.Write("What is your last name? ");
+    String last_name = Console.ReadLine();
+    Console.Write($"Your name is {name}, {last_name} {name}.");
+  }
 }
