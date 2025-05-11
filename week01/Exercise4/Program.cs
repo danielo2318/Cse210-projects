9ad5af40f54c5b1df1631c074d7ea29ecb767e9e
danielo2318 <aroctaviodaniel9@gmail.com>
2025-05-10 21:06:41 -0600

    max number

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+    List<int> numbers = new List<int>();
+    int userNumber;
+
+    do
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+      Console.WriteLine("Enter a list of numbers, type 0 when finished");
+      userNumber = int.Parse(Console.ReadLine());
+
+      if (userNumber != 0)
+      {
+        numbers.Add(userNumber);
+      }
+    } while (userNumber != 0);
+
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+      sum += number;
     }
+    Console.WriteLine($"The sum is {sum}");
+
+    // El float average = ((float) sum) / numbers.Count; sum se asume que es una variable que contiene la suma de todos los elementos de la coleccion numbers. ((float)sum) es una conversion de tipo Convierte sum (probablemente un entero int) a tipo float, que permite decimales.
+    float average = ((float)sum) / numbers.Count; 
+    Console.WriteLine($"The average is {average}");
+    
+    //se inicia con el primer elemento, se toma como punto de partida para comparar los demas valores.
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+      //Si el numero actual (number) es amyor que el valor almacenado en max, entonces se actualiza max con ese número, asi se asegura que max siempre tenga el valor mas alto encontrado hasta el momento.
+      if (number > max)
+      {
+        max = number;
+      }
+    }
+    Console.WriteLine($"The max number is {max}");
+
+
+  }
 }
